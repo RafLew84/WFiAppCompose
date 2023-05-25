44b7d4c8e3b7285f42b126c932674f0831225e62
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-05-25 22:49:32 +0200

    LazyVerticalGrid + orientation handling

--- app/src/main/java/com/example/wfiappcompose/ui/screens/List.kt
+++ app/src/main/java/com/example/wfiappcompose/ui/screens/List.kt
@@ -1,5 +1,7 @@
 package com.example.wfiappcompose.ui.screens
 
+import android.content.res.Configuration
+import android.graphics.drawable.GradientDrawable.Orientation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,21 +13,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +50,24 @@ fun InstituteList(onClick: (Int) -> Unit) {
         mutableStateOf(DataProvider.institutes.toList())
     }
 
-    LazyColumn(
+    var orientation by remember { mutableStateOf(Configuration.ORIENTATION_PORTRAIT) }
+    val configuration = LocalConfiguration.current
+    var numOfColumns by remember {
+        mutableStateOf(1)
+    }
+
+    LaunchedEffect(configuration) {
+        snapshotFlow { configuration.orientation }
+            .collect { orientation = it }
+    }
+
+    numOfColumns = when (orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> { 1 }
+        else -> { 2 }
+    }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(numOfColumns),
         modifier = Modifier.fillMaxSize()
     ) {
         items(data.size) { index ->
